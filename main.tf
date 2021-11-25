f8803e39a210f4f6130d52afd4e33dac56929d68
Bruno Augusto Bardelli <brunobardelli@gmail.com>
2021-11-25 20:49:02 +0100

    added region

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "application" {
   source  = "app.terraform.io/company-foobar/application/aws"
-  version = "0.0.1"
+  version = "0.0.2"
   ami_name = "packer-linux-aws-tomcat-app"
+  region = var.region
 }
