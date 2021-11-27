267367180892e6d262670f760522e5203bf5f169
Bruno Augusto Bardelli <brunobardelli@gmail.com>
2021-11-27 19:11:45 +0100

    changed image name

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "application" {
   source  = "app.terraform.io/company-foobar/application/aws"
   version = "0.0.2"
-  ami_name = "packer-linux-aws-tomcat-app"
+  ami_name = "packer-linux-aws-python-app"
   region = var.region
 }
