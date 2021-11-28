9ece7bafc6bac75a4d8f28571b56e05fbfce2077
Bruno Augusto Bardelli <brunobardelli@gmail.com>
2021-11-28 11:39:57 +0100

    added default vpc

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
 module "application" {
   source  = "app.terraform.io/company-foobar/application/aws"
   version = "~> 0.0.2"
   ami_name = "packer-linux-aws-python-app"
   region = var.region
-  vpc_id = var.vpc_id
+  vpc_id = aws_default_vpc.default.id
 }
--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "region"{
     type = string
     description = "The region where everything is deployed"
 }
-
-variable vpc_id {
-  description = "VPC ID"
-  type = string
-}
